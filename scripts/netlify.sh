ed1ff9992e744723fa947d33a7e218dbe355b429
Kye Shi <shi.kye@gmail.com>
2021-03-18 08:46:58 -0700

    copy static stuff

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 cp -a 'client/__sapper__/build' 'functions/render'
+cp -a 'client/static' 'functions/render/build'
 sed -i \
   -e 's:"__sapper__/build":path.join(__dirname, ".."):g' \
   'functions/render/build/server/server.js'
