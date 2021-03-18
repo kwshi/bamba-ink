0cb969bb631b7e3347141bea4ae280975c038b23
Kye Shi <shi.kye@gmail.com>
2021-03-18 09:03:08 -0700

    fix script dir

--- client/scripts/netlify.sh
+++ client/scripts/netlify.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/bin/sh -e
 
-cp -a 'client/__sapper__/build' 'functions/render'
-cp -a 'client/static' 'functions/render/build'
+cp -a '__sapper__/build' 'functions/render'
+cp -a 'static' 'functions/render/build'
 sed -i \
   -e 's:"__sapper__/build":path.join(__dirname, ".."):g' \
   'functions/render/build/server/server.js'
-echo '/* /.netlify/functions/render 200' > 'client/__sapper__/build/_redirects'
+echo '/* /.netlify/functions/render 200' > '__sapper__/build/_redirects'
