ae5025b70c5bf91962082a819c361928069db28b
Kye Shi <shi.kye@gmail.com>
2021-03-18 00:52:05 -0700

    add redirects

--- scripts/netlify.sh
+++ scripts/netlify.sh
@@ -4,3 +4,4 @@ cp -a 'client/__sapper__/build' 'functions/render'
 sed -i \
   -e 's:"__sapper__/build":path.join(__dirname, ".."):g' \
   'functions/render/build/server/server.js'
+echo '/* /.netlify/functions/render 200' > 'client/__sapper__/build/_redirects'
