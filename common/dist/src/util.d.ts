b5bd0206c06b58cb4154402adc6c91e95bac2e5d
Kye Shi <shi.kye@gmail.com>
2021-03-18 17:02:07 -0700

    remove committed dist

--- common/dist/src/util.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const hasOwnProperty: <T extends {}, K extends string | number | symbol>(obj: T, prop: K) => obj is T & Record<K, unknown>;
-export declare const isOwnPropertyOf: <T extends {}>(prop: PropertyKey, obj: T) => prop is keyof T;
--- common/dist/src/util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.isOwnPropertyOf = exports.hasOwnProperty = void 0;
-var hasOwnProperty = function (obj, prop) { return obj.hasOwnProperty(prop); };
-exports.hasOwnProperty = hasOwnProperty;
-var isOwnPropertyOf = function (prop, obj) { return obj.hasOwnProperty(prop); };
-exports.isOwnPropertyOf = isOwnPropertyOf;
